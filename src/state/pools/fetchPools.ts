615c42bccbc851606fca8d39e39a993156f747ff
golmaker <iv7.software@gmail.com>
2021-11-20 18:50:12 -0300

    delete logs

--- src/state/pools/fetchPools.ts
+++ src/state/pools/fetchPools.ts
@@ -61,7 +61,7 @@ export const fetchPoolsTotalStaking = async () => {
 
   const nonBnbPoolsTotalStaked = await multicall(cakeABI, callsNonBnbPools)
   const bnbPoolsTotalStaked = await multicall(wbnbABI, callsBnbPools)
-  console.log("TotalStakedNONBNB %o",nonBnbPoolsTotalStaked)
+  // console.log("TotalStakedNONBNB %o",nonBnbPoolsTotalStaked)
   // console.log("TotalStakedBNB %o",bnbPoolsTotalStaked)
 
   return [
