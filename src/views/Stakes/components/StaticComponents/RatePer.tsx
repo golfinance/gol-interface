4c25e8be46499cfffb7fbbc00b1617a518b73d6c
MiracleCorunus <miraclegwon@gmail.com>
2022-05-02 11:12:47 +0900

    fix issues on NFT staking & marketplace feedback

--- src/views/Stakes/components/StaticComponents/RatePer.tsx
+++ src/views/Stakes/components/StaticComponents/RatePer.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Flex, Image, Text } from '@pancakeswap-libs/uikit'
+import { usePriceCakeBusd } from 'state/farms/hooks'
 import { getNumberSuffix } from 'utils/formatBalance'
 
 const RatePer = ({ dailyGolRate }) => {
+  const cakePriceUsd = usePriceCakeBusd()
+
   return (
     <Flex flexDirection="column">
       <Text style={{ textAlign: 'left' }}>Daily Gol / 100 GolPower</Text>
       <Flex mt="12px">
         <Image src="/images/favicon-32x32.png" alt="GolToken" width={32} height={32} />
         <Text color="secondary" fontSize="24px" pr="3px" ml="6px">
-          {getNumberSuffix(dailyGolRate / 1000000, 0)}
+          {getNumberSuffix(dailyGolRate / 1000000, 3)}
         </Text>
         <Text textTransform="uppercase" color="textSubtle" fontSize="18px" style={{ lineHeight: 2 }}>
-          ≈ $0
+          {`≈ $${getNumberSuffix((cakePriceUsd.toNumber() * dailyGolRate) / 1000000, 3)}`}
         </Text>
       </Flex>
     </Flex>
