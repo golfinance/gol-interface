023776e267dc6da96cdfc0526c8bb84cffb28759
Matias Caparros <55497544+caparrosmatias@users.noreply.github.com>
2022-08-22 18:33:49 -0300

    Modify API Endpoint

--- src/config/constants/nftsCollections/golProfilePictures.ts
+++ src/config/constants/nftsCollections/golProfilePictures.ts
@@ -1,7 +1,62 @@
 export const golProfilePictures = [
     {
-      "tokenId": "1",
-      "name": "Handsome Player",
+      "tokenId": "2",
+      "name": "Profile Image 1",
+      "description": "Tiger Warriors are Planet ZUUD's first limited edition 2222 count fully hand-drawn collection on the BNB Chain.",
+      "collectionName": "Bitpunks",
+      "collectionAddress": "0x742466914848c6AB0e7AD36Acd1e4fbf4ee773b1",
+      "image": {
+        "original": "http://localhost:3000/images/players/70.png",
+        "thumbnail": "http://localhost:3000/images/players/70.png",
+        "mp4": null,
+        "webm": null,
+        "gif": null
+      },
+      "attributes": [
+        {
+          "traitType": "Weapon",
+          "value": "None"
+        },
+        {
+          "traitType": "Necklace",
+          "value": "None"
+        },
+        {
+          "traitType": "Mouth",
+          "value": "None"
+        },
+        {
+          "traitType": "Eye",
+          "value": "Green"
+        },
+        {
+          "traitType": "Head",
+          "value": "None"
+        },
+        {
+          "traitType": "Earing",
+          "value": "None"
+        },
+        {
+          "traitType": "Background",
+          "value": "Cider"
+        },
+        {
+          "traitType": "Body",
+          "value": "Magma"
+        },
+        {
+          "traitType": "Cloth",
+          "value": "Gladiator"
+        }
+      ],
+      "collection": {
+        "name": "Planet ZUUD: Tiger Warriors"
+      }
+    },
+    {
+      "tokenId": "2",
+      "name": "Profile Image 2",
       "description": "Tiger Warriors are Planet ZUUD's first limited edition 2222 count fully hand-drawn collection on the BNB Chain.",
       "collectionName": "Bitpunks",
       "collectionAddress": "0x742466914848c6AB0e7AD36Acd1e4fbf4ee773b1",
